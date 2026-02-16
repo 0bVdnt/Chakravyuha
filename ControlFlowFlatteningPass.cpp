c641e19eaae1f5a72ff01a1a22e1d71b51f68cd5
0bVdnt <vedantneve13@gmail.com>
2026-02-16 10:03:45 +0530

    Fixed a build failure on LLVM 22 by updating the PassPlugin include path to 'llvm/Plugins/PassPlugin.h' and removing unused headers in ControlFlowFlatteningPass.

--- ControlFlowFlatteningPass.cpp
+++ ControlFlowFlatteningPass.cpp
@@ -8,8 +8,7 @@
 #include "llvm/IR/Module.h"
 #include "llvm/IR/PassManager.h"
 #include "llvm/Passes/PassBuilder.h"
-#include "llvm/Passes/PassPlugin.h"
-#include "llvm/Support/raw_ostream.h"
+#include "llvm/Plugins/PassPlugin.h"
 #include "llvm/Transforms/Utils/BasicBlockUtils.h"
 #include "llvm/Transforms/Utils/Local.h"
 
